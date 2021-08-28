9e82ca98f61dc2c54955d32358710cec83223faa
L <89287469+eboy11398@users.noreply.github.com>
2021-08-28 12:24:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function rake() {
+    document.getElementById("white").innerHTML = "Hello JavaScript";
+}
+
+function lake() {
+    document.getElementById("white").innerHTML = "This is a random test";
+}
+
+function Anim() {
+    document.getElementById("L").src = "https://i.pinimg.com/736x/db/58/17/db58173e2ecc8ac6de8703b81368037c.jpg";
+
+}
+
+function enim() {
+    document.getElementById("L").src = "https://i.pinimg.com/originals/88/c2/83/88c283c264e2605f9eb15df1146948d4.jpg";
+
+}
+
+function anime() {
+    document.getElementById("L").src = "https://image.winudf.com/v2/image1/Y29tLmJydWhwcm9kLnF1aW50dXBsZXRzaWNoaWthbmFrYW5vX3NjcmVlbl81XzE2MTYzMzM0ODFfMDQz/screen-5.jpg?fakeurl=1&type=.jpg";
+
+}
